d08bc1cf6b902886142b15d31baf978af878a2b3
anishregmi17 <anishregminaglibang@gmail.com>
2024-06-21 12:41:03 +0545

    update content policy

--- resources/views/admin/contentpolicy/show.blade.php
+++ resources/views/admin/contentpolicy/show.blade.php
@@ -1,9 +1,9 @@
 @extends('admin.layouts.main')
 
 @section('content')
-<div class="container mt-3">
+<div class="container mt-5">
     <div class="row justify-content-center">
-        <div class="col-md-8">
+        <div class="col-md-8 mt-5">
             <h1>{{ $contentPolicy->title }}</h1>
             <p>{{ $contentPolicy->content }}</p>
         </div>

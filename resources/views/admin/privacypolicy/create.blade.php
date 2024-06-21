c6521053c2687e60e711be0f92782306d1bf1728
anishregmi17 <anishregminaglibang@gmail.com>
2024-06-21 11:44:46 +0545

    update privacy policy crud

--- resources/views/admin/privacypolicy/create.blade.php
+++ resources/views/admin/privacypolicy/create.blade.php
@@ -1,42 +1,32 @@
-<!doctype html>
-<html lang="en">
-<head>
-    <title>Title</title>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+@extends('admin.layouts.main')
 
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-</head>
-<body>
-<div class="card m-5 p-5">
-    <div class="card-body m-5 p-5">
-        <form action="{{ route('posts.store') }}" method="POST" enctype="multipart/form-data">
-            @csrf
-            <div class="form-group mb-3">
-                <label for="title" class="form-label">Title</label>
-                <input type="text" class="form-control" name="title" id="title" value="{{ old('title') }}">
-                @error('title')
-                <small class="text-danger">{{ $message }}</small>
-                @enderror
-            </div>
-            <div class="form-group mb-3">
-                <label for="description" class="form-label">description</label>
-                <input type="text" class="form-control" name="description" id="description" value="{{ old('description') }}">
-                @error('description')
-                <small class="text-danger">{{ $message }}</small>
-                @enderror
-            </div>
-            <button type="submit" class="btn btn-primary">Save Post</button>
-        </form>
+@section('content')
+<div class="container mt-3">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <h1>Create Privacy Policy</h1>
+            @if($errors->any())
+                <div class="alert alert-danger">
+                    <ul>
+                        @foreach($errors->all() as $error)
+                            <li>{{ $error }}</li>
+                        @endforeach
+                    </ul>
+                </div>
+            @endif
+            <form action="{{ route('admin.privacypolicy.store') }}" method="POST">
+                @csrf
+                <div class="form-group">
+                    <label for="title">Title</label>
+                    <input type="text" name="title" class="form-control" required>
+                </div>
+                <div class="form-group">
+                    <label for="content">Content</label>
+                    <textarea name="content" class="form-control" required></textarea>
+                </div>
+                <button type="submit" class="btn btn-success">Create Privacy Policy</button>
+            </form>
+        </div>
     </div>
 </div>
-
-<!-- Optional JavaScript -->
-<!-- jQuery first, then Popper.js, then Bootstrap JS -->
-<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-</body>
-</html>
+@endsection

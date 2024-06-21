bdc9619e9d52990f94896d446ca9ab0d1a0d7684
anishregmi17 <anishregminaglibang@gmail.com>
2024-06-21 11:27:57 +0545

    aboutforum crud

--- resources/views/admin/aboutforum/show.blade.php
+++ resources/views/admin/aboutforum/show.blade.php
@@ -1,9 +1,9 @@
 @extends('admin.layouts.main')
 
 @section('content')
-<div class="container mt-3">
+<div class="container mt-5">
     <div class="row justify-content-center">
-        <div class="col-md-8">
+        <div class="col-md-8 mt-5">
             <h1>{{ $aboutforum->text }}</h1>
             <p>{{ $aboutforum->description }}</p>
         </div>

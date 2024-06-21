9156340ec24a712fa8d4251a072cc9b9994ccae8
anishregmi17 <anishregminaglibang@gmail.com>
2024-06-21 12:29:08 +0545

    update privacy policy crud

--- resources/views/admin/privacypolicy/show.blade.php
+++ resources/views/admin/privacypolicy/show.blade.php
@@ -1,9 +1,9 @@
 @extends('admin.layouts.main')
 
 @section('content')
-<div class="container mt-3">
+<div class="container mt-5">
     <div class="row justify-content-center">
-        <div class="col-md-8">
+        <div class="col-md-8 mt-5">
             <h1>{{ $privacypolicy->title }}</h1>
             <p>{{ $privacypolicy->content }}</p>
         </div>

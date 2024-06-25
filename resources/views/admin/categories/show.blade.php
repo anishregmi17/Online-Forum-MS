ff66d2cd7f4da775698e5faea0587b5d7b5438e0
anishregmi17 <anishregminaglibang@gmail.com>
2024-06-25 12:06:19 +0545

    update all

--- resources/views/admin/categories/show.blade.php
+++ resources/views/admin/categories/show.blade.php
@@ -4,16 +4,16 @@
 <div class="container mt-5">
     <div class="row justify-content-center">
         <div class="col-md-8 mt-5">
-            <h1>Community Details</h1>
+            <h1>Category Details</h1>
             <div class="form-group">
                 <strong>Title:</strong>
-                {{ $community->title }}
+                {{ $category->title }}
             </div>
             <div class="form-group">
                 <strong>Icon:</strong>
-                {{ $community->icon }}
+                {{ $category->icon }}
             </div>
-            <a href="{{ route('admin.communities.index') }}" class="btn btn-primary">Back to List</a>
+            <a href="{{ route('admin.categories.index') }}" class="btn btn-primary">Back to List</a>
         </div>
     </div>
 </div>

3634b85e5d4ed40e412d0fbd2f2b4634771d3e03
anishregmi17 <anishregminaglibang@gmail.com>
2024-06-25 12:05:58 +0545

    update all

--- resources/views/admin/categories/edit.blade.php
+++ resources/views/admin/categories/edit.blade.php
@@ -4,7 +4,7 @@
 <div class="container mt-5">
     <div class="row justify-content-center">
         <div class="col-md-8 mt-5">
-            <h1>Edit Community</h1>
+            <h1>Edit Category</h1>
             @if($errors->any())
                 <div class="alert alert-danger">
                     <ul>
@@ -14,18 +14,18 @@
                     </ul>
                 </div>
             @endif
-            <form action="{{ route('admin.communities.update', $community->id) }}" method="POST">
+            <form action="{{ route('admin.categories.update', $category->id) }}" method="POST">
                 @csrf
                 @method('PUT')
                 <div class="form-group">
                     <label for="title">Title</label>
-                    <input type="text" name="title" class="form-control" value="{{ $community->title }}" required>
+                    <input type="text" name="title" class="form-control" value="{{ $category->title }}" required>
                 </div>
                 <div class="form-group">
                     <label for="icon">Icon</label>
-                    <input type="text" name="icon" class="form-control" value="{{ $community->icon }}" required>
+                    <input type="text" name="icon" class="form-control" value="{{ $category->icon }}" required>
                 </div>
-                <button type="submit" class="btn btn-success">Update Community</button>
+                <button type="submit" class="btn btn-success">Update Category</button>
             </form>
         </div>
     </div>

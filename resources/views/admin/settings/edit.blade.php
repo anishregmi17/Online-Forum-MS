64d70b5bb417a636539b13e4290192081134975c
anishregmi17 <anishregminaglibang@gmail.com>
2024-06-28 22:33:21 +0545

    update all

--- resources/views/admin/settings/edit.blade.php
+++ resources/views/admin/settings/edit.blade.php
@@ -4,7 +4,7 @@
 <div class="container mt-5">
     <div class="row justify-content-center">
         <div class="col-md-8 mt-5">
-            <h1>Edit Footer</h1>
+            <h1>Edit Setting</h1>
             @if($errors->any())
                 <div class="alert alert-danger">
                     <ul>
@@ -14,25 +14,24 @@
                     </ul>
                 </div>
             @endif
-            <form action="{{ route('admin.footers.update', $footer->id) }}" method="POST">
+            <form action="{{ route('admin.settings.update', $setting->id) }}" method="POST">
                 @csrf
                 @method('PUT')
                 <div class="form-group">
-                    <label for="title">Title</label>
-                    <input
-                        type="text"
-                        name="title"
-                        class="form-control @error('title') is-invalid @enderror"
-                        value="{{ old('title', $footer->title) }}"
-                        required
-                        placeholder="Enter title"
-                    >
-                    @error('title')
+                    <label for="site_key">Key</label>
+                    <input type="text" name="site_key" class="form-control @error('site_key') is-invalid @enderror" value="{{ old('site_key', $setting->site_key) }}" required>
+                    @error('site_key')
                         <div class="invalid-feedback">{{ $message }}</div>
                     @enderror
                 </div>
-
-                <button type="submit" class="btn btn-success">Update Footer</button>
+                <div class="form-group">
+                    <label for="site_value">Value</label>
+                    <input type="text" name="site_value" class="form-control @error('site_value') is-invalid @enderror" value="{{ old('site_value', $setting->site_value) }}" required>
+                    @error('site_value')
+                        <div class="invalid-feedback">{{ $message }}</div>
+                    @enderror
+                </div>
+                <button type="submit" class="btn btn-success">Update Setting</button>
             </form>
         </div>
     </div>

af0f275948e019277c0979abdc74c30d5e26c1dc
anishregmi17 <anishregminaglibang@gmail.com>
2024-06-28 22:33:01 +0545

    update all

---
+++ resources/views/admin/settings/create.blade.php
@@ -0,0 +1,38 @@
+@extends('admin.layouts.main')
+
+@section('content')
+<div class="container mt-5">
+    <div class="row justify-content-center">
+        <div class="col-md-8 mt-5">
+            <h1>Create Setting</h1>
+            @if($errors->any())
+                <div class="alert alert-danger">
+                    <ul>
+                        @foreach($errors->all() as $error)
+                            <li>{{ $error }}</li>
+                        @endforeach
+                    </ul>
+                </div>
+            @endif
+            <form action="{{ route('admin.settings.store') }}" method="POST">
+                @csrf
+                <div class="form-group">
+                    <label for="site_key">Key</label>
+                    <input type="text" name="site_key" class="form-control @error('site_key') is-invalid @enderror" value="{{ old('site_key') }}" required>
+                    @error('site_key')
+                        <div class="invalid-feedback">{{ $message }}</div>
+                    @enderror
+                </div>
+                <div class="form-group">
+                    <label for="site_value">Value</label>
+                    <input type="text" name="site_value" class="form-control @error('site_value') is-invalid @enderror" value="{{ old('site_value') }}" required>
+                    @error('site_value')
+                        <div class="invalid-feedback">{{ $message }}</div>
+                    @enderror
+                </div>
+                <button type="submit" class="btn btn-success">Create Setting</button>
+            </form>
+        </div>
+    </div>
+</div>
+@endsection

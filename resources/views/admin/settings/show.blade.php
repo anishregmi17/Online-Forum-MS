317cb7a57a49e53d6d06ba9e4743d12b9c69740e
anishregmi17 <anishregminaglibang@gmail.com>
2024-06-28 22:34:53 +0545

    update all

--- resources/views/admin/settings/show.blade.php
+++ resources/views/admin/settings/show.blade.php
@@ -4,13 +4,17 @@
 <div class="container mt-5">
     <div class="row justify-content-center">
         <div class="col-md-8 mt-5">
-            <h1>Footer Details</h1>
+            <h1>Setting Details</h1>
             <div class="form-group">
-                <strong>Title:</strong>
-                <p>{{ $footer->title }}</p>
+                <strong>Key:</strong>
+                {{ $setting->site_key }}
+            </div>
+            <div class="form-group">
+                <strong>Value:</strong>
+                {{ $setting->site_value }}
             </div>
 
-            <a href="{{ route('admin.footers.index') }}" class="btn btn-primary mt-3">Back to List</a>
+            <a href="{{ route('admin.settings.index') }}" class="btn btn-primary mt-3">Back to List</a>
         </div>
     </div>
 </div>

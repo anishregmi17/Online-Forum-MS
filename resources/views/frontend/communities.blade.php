0fcfc3210c9d49e47058043eaa574772c88891f4
anishregmi17 <anishregminaglibang@gmail.com>
2024-06-30 12:55:16 +0545

    update all

--- resources/views/frontend/communities.blade.php
+++ resources/views/frontend/communities.blade.php
@@ -1,20 +1,18 @@
-
-        <!-- popular communities -->
-        <div class="col-md-3">
-            <div class="content sticky-sidebar">
-              <h3>Popular Topic Communities</h3>
-              <!-- Popular Communities Content Goes Here -->
-              <!-- Add more popular communities content here -->
-               @foreach($communities as $community)
-              <ul class="list-unstyled">
+<!-- popular communities -->
+<div class="col-md-3">
+    <div class="content sticky-sidebar">
+        <h3>Popular Topic Communities</h3>
+        <!-- Popular Communities Content Goes Here -->
+        <!-- Add more popular communities content here -->
+        @foreach ($communities as $community)
+            <ul class="list-unstyled">
                 <li>
-                  <a href="#">
-                    <span class="font-weight-bold community-name p-2"
-                      >{{$community->title}}-<i class="{{$community->icon}}"></i
-                    ></span>
-                  </a>
+                    <a href="#">
+                        <span class="font-weight-bold community-name p-2">{{ $community->title }}-<i
+                                class="{{ $community->icon }}"></i></span>
+                    </a>
                 </li>
-              </ul>
-              @endforeach
-            </div>
-          </div>
+            </ul>
+        @endforeach
+    </div>
+</div>

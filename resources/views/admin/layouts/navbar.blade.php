1a7a877b8a2254893e03f355788bb1a683c6731b
anishregmi17 <anishregminaglibang@gmail.com>
2024-07-08 23:05:02 +0545

    update all

--- resources/views/admin/layouts/navbar.blade.php
+++ resources/views/admin/layouts/navbar.blade.php
@@ -171,7 +171,7 @@
                  <a class="nav-link nav-profile d-flex align-items-center pe-0" href="#"
                      data-bs-toggle="dropdown">
                      <img src="{{ asset('assets/img/profile-img1.jpeg') }}" alt="" class="rounded-circle">
-                     <span class="d-none d-md-block dropdown-toggle ps-2">Admin</span>
+                     <span class="d-none d-md-block dropdown-toggle ps-2">Anish</span>
                  </a>
 
                  <ul class="dropdown-menu dropdown-menu-end dropdown-menu-arrow profile">
